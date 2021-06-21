08ad21c8e384287c7a43358c3baf9384aef06cad
Asher Hwang <dev@hsh.kr>
2021-06-21 11:51:26 +0900

    Revert "Firebase Google auth 연동"
    This reverts commit 9aa810e696f4b7573e9700c03c0150eb1d9ff25c.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import Firebase
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +7,25 @@ import Firebase
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+      [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/provider/CountProvider.dart';
 import 'package:provider/provider.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
-
-class GoogleAuth extends StatefulWidget {
-  @override
-  _GoogleAuthState createState() => _GoogleAuthState();
-}
-
-class _GoogleAuthState extends State<GoogleAuth> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
-
-  bool _isLoggedIn = false;
-  FirebaseUser _user;
-
-  void _login() async {
-    try {
-      final googleSignInAccount = await _googleSignIn.signIn();
-      final gsa = await googleSignInAccount.authentication;
-      final credential = GoogleAuthProvider.getCredential(
-        idToken: gsa.idToken,
-        accessToken: gsa.accessToken,
-      );
-      final authResult = await _auth.signInWithCredential(credential);
-      _user = authResult.user;
-
-      setState(() => _isLoggedIn = true);
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  void _logout() {
-    _googleSignIn.signOut();
-    setState(() => _isLoggedIn = false);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (null == _user || !_isLoggedIn) {
-      return ElevatedButton(
-        onPressed: _login,
-        child: const Text('구글로 로그인'),
-      );
-    }
-
-    return Column(
-      children: [
-        Image.network(
-          _googleSignIn.currentUser.photoUrl,
-          width: 50,
-          height: 50,
-        ),
-        Text(
-          _googleSignIn.currentUser.displayName,
-        ),
-        ElevatedButton(
-          onPressed: _logout,
-          child: const Text('로그아웃'),
-        ),
-      ],
-    );
-  }
-}
 
 /// You can remove _Screen widget to reset project when you use this template.
 class _Screen extends StatelessWidget {
@@ -76,7 +12,25 @@ class _Screen extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        GoogleAuth(),
+        Center(
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset(
+              'images/moa.jpg',
+              width: 100,
+              height: 100,
+            ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Center(
+            child: Text(
+              'Hello Flutter!',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
       ],
     );
   }
